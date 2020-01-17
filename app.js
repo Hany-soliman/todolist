d40b89f367cac9704d411163514ac98eac00f83a
Hany Soliman <Hany.essam1993@gmail.com>
2020-01-17 08:48:39 +0200

    NA

--- app.js
+++ app.js
@@ -156,11 +156,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-
-
-app.listen(port, function() {
-  console.log("Server started Successfully");
-});
+app.listen(port);
 
 
 
